cbfa0312542bf77bbbce70b6e96474367740043d
ravi122004 <raviyaduvanshispecial@gmail.com>
2026-04-27 11:41:58 +0530

    Implementing heap

--- Heaps/intro.cpp
+++ Heaps/intro.cpp
@@ -1,1 +1,29 @@
  
+#include<iostream>
+using namespace std;
+class maxHeap{
+    public: 
+    int arr[50];
+    int idx;
+    maxHeap(){
+        idx=1;
+    }
+    int top(){
+        return arr[1];
+    }
+    int push(int x){
+        arr[idx]=x;
+        int i=idx;
+        idx++;
+        while(i!=1){
+            int parent=i/2;
+            if(arr[i]<arr[parent]){
+                swap(arr[i],arr[parent]);
+            }
+            else break;
+        }
+    }
+};
+int main(){
+
+}
